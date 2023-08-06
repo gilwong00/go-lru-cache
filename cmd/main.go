70eff2ff774cb649df6a9b44d6b282ec9b514033
Gilbert <gilwong3105@gmail.com>
2023-08-06 01:19:33 -0700

    Fix main package and initialize cache instance

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
-package cmd
+package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gilwong00/go-lru-cache/pkg/lrucache"
+	"github.com/gilwong00/go-lru-cache/pkg/queue"
+)
 
 func main() {
 	fmt.Println("CACHE INITIALIZE")
+	lruCache := lrucache.NewLruCache(10)
+
+	// Add
+	err := lruCache.Add(&queue.Node{Val: "test"})
+	if err != nil {
+		fmt.Printf("error adding to cache: %v", err)
+		panic(err)
+	}
+
+	// print cache values
+
+	// Remove
+
+	// print
 }
